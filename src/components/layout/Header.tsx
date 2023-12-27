d7a0061f5962e122145ae563c71b9215c326d26e
dosion9 <dosion9ood@gmail.com>
2023-12-27 14:16:15 +0900

    update : header Nav 생성

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container m-auto">Header</div>
+    <header className="bg-purple-900 py-4 text-white font-bold">
+      <div className="container m-auto flex flex-row">
+        <nav className="flex flex-row gap-2">
+          <Link href={"/"} className="mr-8">
+            로고
+          </Link>
+          <Link href={"/map"}>지도</Link>
+          <Link href={"/auth"}>로그인</Link>
+          <Link href={"/form"}>폼</Link>
+        </nav>
+
+        <div className="ml-auto ">login</div>
+      </div>
     </header>
   );
 };
