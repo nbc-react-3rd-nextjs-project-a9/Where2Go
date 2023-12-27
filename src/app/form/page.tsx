0821ffdf9d61d35eb52cc95b2d4a3593e9b92bd5
hyewon-han <hw1635@hanmail.net>
2023-12-27 17:10:19 +0900

    add: 이미지 드래그앤 드롭 기능 추가 (#1)

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,21 +1,29 @@
-// pages/form.tsx
 "use client";
+import ImageUploader from "@/components/form/ImageUploader";
 import { supabase } from "@/lib/supabase";
-import { useEffect } from "react";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
 
 const FormPage = () => {
-  useEffect(() => {
-    const insertData = async () => {
-      const { error } = await supabase.from("posts").insert([{ id: 1, name: "Denmark" }]);
-      if (error) {
-        throw error;
-      }
-    };
-
-    insertData();
-  }, []);
-
-  return <div>FormPageTest</div>;
+  const handleUpload = (files: File[]) => {
+    console.log("업로드된 파일:", files);
+  };
+  // useEffect(() => {
+  //   const insertData = async () => {
+  //     const { error } = await supabase.from("posts").insert([{ id: 1, name: "Denmark" }]);
+  //     if (error) {
+  //       throw error;
+  //     }
+  //   };
+  //   insertData();
+  // }, []);
+  return (
+    <>
+      <h2>사진선택</h2>
+      <ImageUploader onUpload={handleUpload} />
+      <h2></h2>
+    </>
+  );
 };
 
 export default FormPage;
