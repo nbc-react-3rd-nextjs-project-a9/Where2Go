3e38b41bfd7d3f9401f51a1316a508b4a7030568
yesol <cloud.mys.99@gmail.com>
2023-12-27 20:56:31 +0900

    before pull

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,14 @@
+// "use client";
 import React from "react";
+import { signInWithKakao } from "@/components/auth/authService";
+import Login from "@/components/auth/Login";
 
 const authPage = () => {
-  return <div>authPage</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default authPage;
---
+++ src/lib/supabase/index.ts
@@ -0,0 +1,3 @@
+export * from "./supabase";
+
+// import {supabase} from "~/lib/supabase"로 supabase 클라이언트 불러올 수 있음
