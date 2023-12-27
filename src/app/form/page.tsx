aa140fc4c2b1b99c047d37311d3912d6040310e7
hyewon-han <hw1635@hanmail.net>
2023-12-27 15:33:55 +0900

    update: supabase 연결 (#1)

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+// pages/form.tsx
+"use client";
+import { supabase } from "@/lib/supabase";
+import { useEffect } from "react";
 
 const FormPage = () => {
+  useEffect(() => {
+    const insertData = async () => {
+      const { error } = await supabase.from("posts").insert([{ id: 1, name: "Denmark" }]);
+      if (error) {
+        throw error;
+      }
+    };
+
+    insertData();
+  }, []);
+
   return <div>FormPageTest</div>;
 };
 
