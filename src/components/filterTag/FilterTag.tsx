e410e66d137f93b902772cd135bb00e3bf074da6
dosion9 <dosion9ood@gmail.com>
2023-12-28 11:30:31 +0900

    refact : filterTagList 리팩토링

--- src/components/filterTag/FilterTag.tsx
+++ src/components/filterTag/FilterTag.tsx
@@ -1,21 +1,28 @@
-import { enumCategory, enumFilter } from "@/types/enum";
+"use client";
+
 import React from "react";
 
 interface FilterTagProps {
-  tag: keyof typeof enumCategory | keyof typeof enumFilter;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FilterTag = ({ tag }: FilterTagProps) => {
-  const tagValue =
-    tag in enumCategory ? enumCategory[tag as keyof typeof enumCategory] : enumFilter[tag as keyof typeof enumFilter];
+const FilterTag = ({ value, onChange }: FilterTagProps) => {
   return (
     <>
       <label
         className="cursor-pointer  px-3 py-1 mr-2 border-purple-900  border-2 rounded-full has-[:checked]:bg-purple-900 has-[:checked]:text-white"
-        htmlFor={`filterTag-${tag}`}
+        htmlFor={`filterTag-${value}`}
       >
-        {tagValue}
-        <input className="hidden" type="radio" name={`filterTag`} id={`filterTag-${tag}`} />
+        {value}
+        <input
+          className="hidden"
+          type="radio"
+          name={`filterTag`}
+          id={`filterTag-${value}`}
+          value={value}
+          onChange={onChange}
+        />
       </label>
     </>
   );
