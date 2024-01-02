773e13b64fbde61217f766922d05fe074979421c
dosion9 <dosion9ood@gmail.com>
2024-01-02 20:41:45 +0900

    fix : 북마크 로그인 시 버그 수정

--- src/app/posts/[placeId]/[userId]/Bookmark.tsx
+++ src/app/posts/[placeId]/[userId]/Bookmark.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useBookmark from "@/hooks/useBookmark";
+import useLogedInStore from "@/store/logedInStore";
 import React, { useEffect } from "react";
 
 interface Props {
@@ -12,16 +13,19 @@ enum BookmarkEnum {
 }
 
 const Bookmark = ({ placeId }: Props) => {
+  const { logedIn } = useLogedInStore();
+  if (!logedIn) return;
   const userId = sessionStorage.getItem("uid");
   if (!userId) return;
-  const { data, addBookmark, isBookmark, deleteBookmark } = useBookmark({ placeId: placeId, userId: userId });
+
+  const { addBookmark, isBookmark, deleteBookmark } = useBookmark({ placeId: placeId, userId: userId });
 
   return (
     <div className="absolute left-4 top-0 translate-y-[-3px] z-10" onClick={!isBookmark ? addBookmark : deleteBookmark}>
       <div
         className={`"border-gray-400" ${
           isBookmark && BookmarkEnum["enable"]
-        } relative h-[80px] w-[50px]   border-b-transparent border-l-[25px] border-r-[25px] border-b-[25px]`}
+        } relative h-[80px] w-[50px]   border-b-transparent border-l-[25px] border-r-[25px] border-b-[25px] cursor-pointer`}
       ></div>
     </div>
   );
