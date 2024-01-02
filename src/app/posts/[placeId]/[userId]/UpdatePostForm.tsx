a2a9817add570a5096c6744875bdc5927ffb8c5f
hyewon-han <hw1635@hanmail.net>
2024-01-03 00:46:42 +0900

    update : 수정 기능 작동 함! 방문날짜 추가 (#1)

--- src/app/posts/[placeId]/[userId]/UpdatePostForm.tsx
+++ src/app/posts/[placeId]/[userId]/UpdatePostForm.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
+import { Dispatch, SetStateAction, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
@@ -17,9 +17,13 @@ interface UpdatePostFormProps {
     imageUrlList: string[];
   };
   onCancel: () => void;
+  placeId: string;
+  setIsEditing: Dispatch<SetStateAction<boolean>>;
+  placeData: string[];
 }
 
-const UpdatePostForm = ({ initialData, onCancel }: UpdatePostFormProps) => {
+const UpdatePostForm = ({ initialData, onCancel, placeId, setIsEditing, placeData }: UpdatePostFormProps) => {
+  console.log("플레이스데이터", placeData);
   const { register, handleSubmit, setValue } = useForm({
     defaultValues: {
       content: initialData.content
@@ -27,12 +31,17 @@ const UpdatePostForm = ({ initialData, onCancel }: UpdatePostFormProps) => {
   });
   const router = useRouter();
 
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async (data: { content: string }) => {
       await supabase.from("placeReview").update(data).eq("placeReviewId", initialData.placeReviewId);
     },
-    onSuccess: () => {
-      router.push(`/posts/${initialData.placeId}/${initialData.userId}`);
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["placeReview", placeId]
+      });
+      router.push(`/posts/${placeId}/${initialData.userId}`);
+      setIsEditing(false);
     }
   });
 
@@ -42,8 +51,7 @@ const UpdatePostForm = ({ initialData, onCancel }: UpdatePostFormProps) => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* 수정할 내용을 입력하는 폼 */}
-      <textarea {...register("content")} />
+      <textarea {...register("content")} className="px-2 py-1 border-2 border-purple-900" />
       <Button type="submit">수정하기</Button>
       <Button onClick={onCancel}>취소</Button>
     </form>
--- src/app/posts/[placeId]/[userId]/page.tsx
+++ src/app/posts/[placeId]/[userId]/page.tsx
@@ -29,8 +29,6 @@ const PostPage = () => {
   const [isEditing, setIsEditing] = useState(false);
   // const uid = sessionStorage.getItem("uid");
   const { logedIn } = useLogedInStore();
-  // console.log(placeId);
-  // console.log("목유저데이터", mockUserData);
 
   const handleEditClick = () => {
     setIsEditing(true);
@@ -44,20 +42,14 @@ const PostPage = () => {
     queryKey: ["place"],
     queryFn: () => getPlaceDataByPlaceId(placeId)
   });
-  // console.log("플레이스데이터 한개", placeData);
-
-  // queryKey 추가
   const { data: placeReviewData, isLoading: isPlaceReviewDataLoading } = useQuery({
-    queryKey: ["placeReview", placeData],
+    queryKey: ["placeReview", placeData?.placeId],
     queryFn: () => getPlaceReviewsDataByPlaceName(placeData.placeName),
     enabled: !!placeData
   });
   // console.log("플레이스 리뷰 데이타!", placeReviewData);
 
   const userIds = placeReviewData?.map((data) => data.userId) || [];
-  // console.log("userIds", userIds);
-
-  // isLoading 옵션 추가, queryKey 추가
 
   const { data: userData, isLoading: isUserDataLoading } = useQuery({
     queryKey: ["users", placeReviewData],
@@ -71,9 +63,9 @@ const PostPage = () => {
   const placeReviewDataByUserId = placeReviewData?.filter((data) => data.userId === userId);
   console.log("플레이스리뷰데이터바이유저아이디", placeReviewDataByUserId);
   const placeReviewId = placeReviewDataByUserId && placeReviewDataByUserId[0]?.placeReviewId;
-  console.log("placeReviewId", placeReviewId);
+
   const imageUrlList = placeReviewDataByUserId && placeReviewDataByUserId[0]?.imageUrlList;
-  console.log("imageUrlList", imageUrlList);
+
   // 같은 장소에 리뷰를 쓴 유저들 중 현재 페이지에 맞는 user 정보
   const selectedUser = userData?.find((user) => user.id === userId);
 
@@ -85,7 +77,6 @@ const PostPage = () => {
       publicUrls.push(data.publicUrl);
     }
   }
-  console.log("publicUrls", publicUrls);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -133,7 +124,13 @@ const PostPage = () => {
   };
   if (isEditing) {
     return (
-      <UpdatePostForm initialData={placeReviewDataByUserId && placeReviewDataByUserId[0]} onCancel={handleCancelEdit} />
+      <UpdatePostForm
+        initialData={placeReviewDataByUserId && placeReviewDataByUserId[0]}
+        onCancel={handleCancelEdit}
+        placeId={placeId}
+        setIsEditing={setIsEditing}
+        placeData={placeData}
+      />
     );
   }
 
@@ -187,6 +184,9 @@ const PostPage = () => {
               <p className="">{placeReviewDataByUserId && placeReviewDataByUserId[0]?.content}</p>
             </>
           }
+        </Section>
+        <Section title="방문 날짜">
+          <p className="">{placeReviewDataByUserId && placeReviewDataByUserId[0]?.visitedAt}</p>
         </Section>
         <Section title="주소">
           <p>{placeData?.address}</p>
