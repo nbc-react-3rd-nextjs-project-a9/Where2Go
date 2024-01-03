7e2be269dea90421d2a7e8d95d4403a57df887cb
dosion9 <dosion9ood@gmail.com>
2024-01-03 09:56:48 +0900

    fix : not a valid Route export field.

--- src/app/api/places/route.ts
+++ src/app/api/places/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { supabase } from "@/lib/supabase";
 
-export const getPlaceData = async (): Promise<Place[] | null> => {
+const getPlaceData = async () => {
   const { data } = await supabase.from("places").select();
   if (typeof data === null) return [];
   else return data;
