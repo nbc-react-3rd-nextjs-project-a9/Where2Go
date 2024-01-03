414e988a558e5de937b3f8a780db3172d874745e
dosion9 <dosion9ood@gmail.com>
2024-01-03 10:28:55 +0900

    delete : dummy export 삭제

--- src/app/user/[userId]/UserReview.tsx
+++ src/app/user/[userId]/UserReview.tsx
@@ -1,7 +1,6 @@
 "use client";
 import PostCardList from "@/components/PostCardList";
 import React from "react";
-import { mockPlaceData } from "@/data/mockPlace";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { getPlaceReviewsDataByUserId } from "@/api/places";
